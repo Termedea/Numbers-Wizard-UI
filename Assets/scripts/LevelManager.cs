484f6962df5ae09764d88f69959fb248ca7d4677
Termedea <mikaela.hedberg@gmail.com>
2016-09-11 13:56:35 +0200

    Init

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+    //Needs to be public to be accessed by Unity
+    public void LoadLevel(string levelName)
+    {
+        Debug.Log("Level load requested for " + levelName);
+        Application.LoadLevel(levelName);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+}
